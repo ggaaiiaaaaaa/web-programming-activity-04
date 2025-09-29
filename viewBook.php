133728904627e78c2cb81fa8f2b63b736ffceeaf
gaiaaaaaaaaa <199040716+ggaaiiaaaaaa@users.noreply.github.com>
2025-09-30 00:02:11 +0800

    Update viewBook.php

--- viewBook.php
+++ viewBook.php
@@ -1,11 +1,15 @@
 <?php
-require_once "../classes/library.php";
+require_once "../classes/book.php";
 $libraryObj = new Library();
 
-$search = isset($_GET['search']) ? trim($_GET['search']) : '';
-$filter = isset($_GET['genre']) ? trim($_GET['genre']) : '';
+$search = "";
+$genre = "";
 
-$books = $libraryObj->viewBook($search, $filter);
+if($_SERVER["REQUEST_METHOD"] == "GET"){
+    $search = isset($_GET["search"])? trim(htmlspecialchars($_GET["search"])) : "";
+    $genre = isset($_GET["genre"])? trim(htmlspecialchars($_GET["genre"])) : "";
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -17,45 +21,42 @@ $books = $libraryObj->viewBook($search, $filter);
 </head>
 <body>
     <h1>List of Books</h1>
-    <button><a href="addBook.php">Add Another Book</a></button>
-
-    <form method="get" action="">
-        <input type="text" name="search" placeholder="Search by title or author" value="<?= htmlspecialchars($search) ?>">
-        <select name="genre">
-            <option value="">-- Genres --</option>
-            <option value="History" <?= ($filter == "History") ? "selected" : "" ?>>History</option>
-            <option value="Science" <?= ($filter == "Science") ? "selected" : "" ?>>Science</option>
-            <option value="Fiction" <?= ($filter == "Fiction") ? "selected" : "" ?>>Fiction</option>
+    <form action="" method="get">
+        <input type="search" name="search" id="search" value="<?= $search ?>">
+        <select name="genre" id="genre">
+                    <option value="" <?= empty($genre) ? "selected" : ""; ?>>All</option>
+                    <option value="History" <?= $genre == "History" ? "selected" : ""; ?>>History</option>
+                    <option value="Science" <?= $genre == "Science" ? "selected" : ""; ?>>Science</option>
+                    <option value="Fiction" <?= $genre == "Fiction" ? "selected" : ""; ?>>Fiction</option>
         </select>
-        <button type="submit">Search</button>
-        <button type="button" onclick="window.location='viewBook.php'">Reset</button>
+        <input type="submit" value="Search">
     </form>
-
+    <button><a href="addBook.php">Add another book</a></button>
+    
     <br>
+
     <table border="1" cellpadding="8" cellspacing="0">
         <tr>
-            <th>No.</th>
+            <th>ID</th>
             <th>Title Name</th>
             <th>Author</th>
             <th>Genre</th>
             <th>Publication Year</th>
+            <th>Action</th>
         </tr>
 
         <?php
         $id = 1;
-        if (!empty($books)) {
-            foreach ($books as $book) {
-                echo "<tr>
-                        <td>{$id}</td>
-                        <td>{$book['title']}</td>
-                        <td>{$book['author']}</td>
-                        <td>{$book['genre']}</td>
-                        <td>{$book['publication_year']}</td>
-                    </tr>";
-                $id++;
-            }
-        } else {
-            echo "<tr><td colspan='5'>Book not found</td></tr>";
+        foreach($libraryObj->viewBook($search, $genre) as $book){
+        ?>
+        <tr>
+            <td><?= $id++ ?></td>
+            <td><?= $book["title"] ?></td>
+            <td><?= $book["author"]  ?></td>
+            <td><?= $book["genre"]  ?></td>
+            <td><?= $book["publication_year"] ?></td>
+        </tr>
+        <?php
         }
         ?>
     </table>
